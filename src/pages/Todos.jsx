ef4fce38d13e0b4185ece02856c404dc4b5db0c2
Влад Язиков <vladzaraza@gmail.com>
2023-10-16 20:04:01 +0300

    Update Todos.jsx

--- src/pages/Todos.jsx
+++ src/pages/Todos.jsx
@@ -2,6 +2,6 @@ import { Description } from "../components/Description/Description";
 
 export const Todos = () => {
   const header = "Todos";
-  const text = "Todos are in progress...Check it later";
+  const text = "Todos are in progress...Check it later!";
   return <Description headertext={header} partext={text} />;
 };
