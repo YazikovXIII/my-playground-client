3a5a9cc63ee70791b96b366423aeac055321337a
Влад Язиков <vladzaraza@gmail.com>
2023-11-04 16:13:14 +0200

    img fix

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -1,19 +1,25 @@
+import IconShownImg from "src/assets/SVG/eye-closed-svgrepo-com.svg";
+import IconHiddenImg from "src/assets/SVG/eye-watch-opened-public-svgrepo-com.svg";
+import IconGitHubImg from "src/assets/SVG/iconmonstr-github-1.svg";
+import IconLinkedInImg from "src/assets/SVG/iconmonstr-linkedin-3.svg";
+import IconCVImg from "src/assets/SVG/iconmonstr-cv-3.svg";
+
 export const IconShown = () => {
-  return <img src="src/assets/SVG/eye-closed-svgrepo-com.svg" alt="show" />;
+  return <img src={IconShownImg} alt="show" />;
 };
 
 export const IconHidden = () => {
-  return <img src="src/assets/SVG/eye-watch-opened-public-svgrepo-com.svg" alt="hide" />;
+  return <img src={IconHiddenImg} alt="hide" />;
 };
 
 export const IconGitHub = () => {
-  return <img src="src/assets/SVG/iconmonstr-github-1.svg" alt="Github" />;
+  return <img src={IconGitHubImg} alt="Github" />;
 };
 
 export const IconLinkedIn = () => {
-  return <img src="src/assets/SVG/iconmonstr-linkedin-3.svg" alt="LinkedIn" />;
+  return <img src={IconLinkedInImg} alt="LinkedIn" />;
 };
 
 export const IconCV = () => {
-  return <img src="src/assets/SVG/iconmonstr-cv-3.svg" alt="CV" />;
+  return <img src={IconCVImg} alt="CV" />;
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,35 @@
+// import { useSelector } from "react-redux";
+// import { Description } from "../components/Description/Description";
+// import { getUser, isLoggedIn } from "../redux/user/authSelectors";
+// import { HomeWrapper, StyledDetailsLink, StyledPhoto } from "./Home.styled";
+
+// export const Home = () => {
+//   const user = useSelector(getUser);
+//   const isUserLoggedIn = useSelector(isLoggedIn);
+//   let username = user.username;
+
+//   const headerUnauth = "Hello!";
+//   const textUnauth = "This page is my playground.Registrate a user and sign in to see more.";
+
+//   const headerIsAuth = `Hello, ${username}!`;
+//   const link = <StyledDetailsLink to="/about">here</StyledDetailsLink>;
+//   const textIsAuth = `My name is Vlad. I am a full-stack JavaScript developer seeking new professional opportunities. This page is my playground. I made this client to learn more about React.js, React-Redux, different libraries etc... Also I made a server on Node.js to work with this page. Github links and more details  `;
+
+//   return isUserLoggedIn ? (
+//     <HomeWrapper>
+//       <StyledPhoto src="src/assets/Img/Hero_photo/1.JPG" alt="My portrait"></StyledPhoto>
+//       <Description headertext={headerIsAuth} partext={textIsAuth} link={link} />
+//     </HomeWrapper>
+//   ) : (
+//     <Description headertext={headerUnauth} partext={textUnauth} />
+//   );
+// };
+
 import { useSelector } from "react-redux";
 import { Description } from "../components/Description/Description";
 import { getUser, isLoggedIn } from "../redux/user/authSelectors";
 import { HomeWrapper, StyledDetailsLink, StyledPhoto } from "./Home.styled";
+import HeroPhoto from "src/assets/Img/Hero_photo/1.JPG"; // Імпортуйте зображення таким чином
 
 export const Home = () => {
   const user = useSelector(getUser);
@@ -17,7 +45,7 @@ export const Home = () => {
 
   return isUserLoggedIn ? (
     <HomeWrapper>
-      <StyledPhoto src="src/assets/Img/Hero_photo/1.JPG" alt="My portrait"></StyledPhoto>
+      <StyledPhoto src={HeroPhoto} alt="My portrait"></StyledPhoto>
       <Description headertext={headerIsAuth} partext={textIsAuth} link={link} />
     </HomeWrapper>
   ) : (
