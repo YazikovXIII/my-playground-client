b93800c7cb9cf663ce7bebafaabb3ac1a73201aa
Влад Язиков <vladzaraza@gmail.com>
2023-11-15 09:53:30 +0200

    svg

--- src/components/Header/Logo/Logo.jsx
+++ src/components/Header/Logo/Logo.jsx
@@ -4,6 +4,8 @@ import { StyledLogoButton, StyledLogoLink, StyledLogoPar } from "./Logo.styled";
 import { useNavigate } from "react-router-dom";
 import "./Logo.scss";
 import { ReactSVG } from "react-svg";
+import enterSvg from "/assets/SVG/enter-svgrepo-com.svg";
+import burgerSVG from "/assets/SVG/burger-checklist-list-menu-navigation-svgrepo-com.svg";
 
 export const Logo = () => {
   return (
@@ -66,7 +68,7 @@ export const BtnAuth = ({ toggle }) => {
       className="enter_button"
     >
       <ReactSVG
-        src="/assets/SVG/enter-svgrepo-com.svg"
+        src={enterSvg}
         beforeInjection={(svg) => {
           svg.classList.add("enter_svg");
         }}
@@ -84,7 +86,7 @@ export const BtnBurger = ({ toggle }) => {
       className="enter_button"
     >
       <ReactSVG
-        src="/assets/SVG/burger-checklist-list-menu-navigation-svgrepo-com.svg"
+        src={burgerSVG}
         beforeInjection={(svg) => {
           svg.classList.add("enter_svg");
         }}
--- src/redux/axios.js
+++ src/redux/axios.js
@@ -1,3 +1,5 @@
 export const API_URL = "https://my-playground-server.onrender.com";
+// export const API_URL = "http://localhost:5000";
+
 // "https://my-playground-server.onrender.com"
 // "http://localhost:5000"
